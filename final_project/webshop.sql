b80a54cfe670ebea644322137a189359b2d23a7a
Matija <73955600+matijaivic@users.noreply.github.com>
2020-12-09 16:58:11 +0100

    Add files via upload

--- final_project/webshop.sql
+++ final_project/webshop.sql
@@ -5,14 +5,6 @@ CREATE database order_store CHARACTER SET utf8;
 
 USE order_store;
 
-CREATE TABLE users (
-	user_id 	 int not null primary key auto_increment, 
-	user_name	 varchar(100) not null, # ime kupca, ime firme..
-	user_type	 varchar(25) not null, # može biti kupac, prodavatelj, dobavljač, zaposlenik..  
-	address		 varchar(200) not null, # navodi se što je potrebno pod adresu
-	phone_number varchar(15)
-);
-
 create table products (
 	product_id 			int not null primary key auto_increment, 
 	product_name 		varchar(100) not null, # naziv proizvoda
@@ -26,23 +18,28 @@ create table sales (
 	sales_id		int not null primary key auto_increment,
 	total_price		decimal(18,2) not null,
 	sales_type 		boolean, # pravna (osoba) ili privatno prodaja
-	quantity 		int, CHECK (quantity > 0), # količina
-	product_fk 		int not null,
-	seller_fk 		int not null,
+	quantity 		int, CHECK (quantity > 0) # količina
+
+);
+
+CREATE TABLE users (
+	user_id 	 	int not null primary key auto_increment, 
+	user_name	 	varchar(100) not null, # ime kupca, ime firme..
+	user_type	 	varchar(25) not null, # može biti kupac, prodavatelj, dobavljač, zaposlenik..  
+	address		 	varchar(200) not null, # navodi se što je potrebno pod adresu
+	phone_number 	varchar(15),	
+	sales_fk		int not null,
 	
-	foreign key (product_fk) references products(product_id),
-	foreign key (seller_fk) references users(user_id)
+	foreign key (sales_fk) references sales(sales_id)
 );
 
 create table orders ( 
-	order_id 				int not null primary key auto_increment,
-	order_date				datetime not null, # datum narudžbe
-	bought_quantity			int not null, CHECK (bought_quantity > 0), # cijena
-	sales_fk				int not null,
-	buyer_fk				int not null,
+	order_id 			int not null primary key auto_increment,
+	order_date			datetime not null, # datum narudžbe
+	bought_quantity		int not null, CHECK (bought_quantity > 0), # cijena
+	product_fk 			int not null,
+	buyer_fk			int not null,
 	
-	foreign key (sales_fk) references sales(sales_id),
-	foreign key (buyer_fk) references users(user_id)
+	foreign key (buyer_fk) references users(user_id),
+	foreign key (product_fk) references products(product_id)
 );
-
-
