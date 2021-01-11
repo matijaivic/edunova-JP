ba5f143bbae6c8e28144f1062d548194115d221b
Matija <73955600+matijaivic@users.noreply.github.com>
2021-01-11 17:23:16 +0100

    Delete Zadatak05.java

--- java/winter_homework/ZimskiZadaci/src/zimskazadaca/Zadatak05.java
+++
@@ -1,9 +0,0 @@
-package zimskazadaca;
-
-public class Zadatak05 {
-	// Kreirati program koji ispisuje cikličnu matricu (predložak u prilogu) - ulaz su dva cijela broja
-	
-	public static void main(String[] args) {
-		
-	}
-}
